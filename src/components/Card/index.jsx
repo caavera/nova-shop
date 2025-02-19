0b9f9e8c7dbcc02604e4e8e51253b053813951e9
CarlosVera <veracar111@gmail.com>
2025-02-19 11:39:32 -0500

    se consume la FakeStore API para pintar cards de productos

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,22 +1,22 @@
 import { FaPlus } from "react-icons/fa";
 
-const Card = () => {
+const Card = ({product}) => {
     return (
         <div className="bg-white cursor-pointer w-56 h-60 rounded-2xl border border-gray-200 shadow-lg hover:shadow-xl transition-shadow transform hover:scale-102">
             <figure className="relative w-full h-4/5">
                 <img className="w-full h-full object-cover rounded-t-2xl"
-                    src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    src={product.images[0]}
                     alt="headphones" />
                 <span className="absolute bottom-2 left-2 bg-black/70 text-white text-xs px-3 py-1 rounded-full">
-                    Electronics
+                    {product.category.name}
                 </span>
                 <button className="absolute top-2 right-2 bg-white hover:bg-blue-300 shadow-md flex items-center justify-center w-6 h-6 rounded-full p-1">
                     <FaPlus size={12} />
                 </button>
             </figure>
-            <div className="flex justify-between items-center mt-3 px-2">
-                <span className="text-sm font-semibold truncate">Wireless Headphones</span>
-                <span className="text-lg font-bold">$300</span>
+            <div className="flex justify-between items-center mt-3 px-2 space-x-2">
+                <span className="text-sm truncate">{product.title}</span>
+                <span className="text-lg font-bold">${product.price}</span>
             </div>
         </div>
     );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,35 @@
+import {useState, useEffect} from 'react'
 import Layout from '../../components/Layout'
 import Card from '../../components/Card'
 
 function Home() {
+  const [items,setItems] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://api.escuelajs.co/api/v1/products');
+        if (!response.ok) throw new Error(`Error ${response.status}: ${response.statusText}`);
+        
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <Layout>
       Home
-      <Card/>
+      <div className='grid gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-screen-lg space-y-3 justify-center'>
+        {
+          items?.map( item => (
+            <Card key={item.id} product={item} />
+          ))
+        }
+      </div>
     </Layout> 
   )
 }
